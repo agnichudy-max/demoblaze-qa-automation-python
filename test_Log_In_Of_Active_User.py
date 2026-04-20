6c41379b1024d989e2b9b56069f5fbd47a8e0402
Agnieszka Chudy <agni.chudy@gmail.com>
2026-04-20 20:02:06 +0200

    initial test checking connection to tested website and if site is running added Test Scenarios for Signing up of a new user (TC_105-TC_107) added Test Scenerios for Log in of an active user (TC_201-TC_204)

---
+++ test_Log_In_Of_Active_User.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+##from data_tools import *
+from time import sleep
+import unittest
+
+# creating test setup
+class TestPageTitle(unittest.TestCase):
+    def setUp(self):
+        # Preconditions
+        # 1. Open the home page
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get('https://www.demoblaze.com/index.html')
+        self.driver.implicitly_wait(15)
+
+# checking that the homepage title is STORE
+    def test_title(self):
+        actual_title = self.driver.title
+        expected_title = 'STORE'
+        self.assertEqual(actual_title, expected_title)
