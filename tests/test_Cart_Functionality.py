9065e9f8cba600c72bb1d122d4645e177d19b0c0
Agnieszka Chudy <agni.chudy@gmail.com>
2026-04-25 21:17:25 +0200

    added TC_501 TC_502 TC_503

--- tests/test_Cart_Functionality.py
+++ tests/test_Cart_Functionality.py
@@ -1,6 +1,7 @@
 import unittest
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common import alert
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -73,6 +74,55 @@ class Test_Cart_functionality(unittest.TestCase):
         # get Pop up text
         alert_text = alert.text
         self.assertEqual(alert_text, expected_message)
+
+    def test_TC_503_View_Cart_With_One_Added_Product(self):
+
+        # step 1 of the TC_503
+        monitor_link = self.driver.find_element(By.LINK_TEXT, "Monitors")
+        monitor_link.click()
+        sleep(2)
+
+        #  step 2 of the TC_503
+        apple_monitor_link = self.driver.find_element(By.LINK_TEXT, "Apple monitor 24")
+        apple_monitor_link.click()
+        sleep(2)
+
+        # step 3 of the TC_503
+        add_to_card_button = self.driver.find_element(By.LINK_TEXT, "Add to cart")
+        add_to_card_button.click()
+        sleep(2)
+
+        # define element that contains the pop-up
+        alert = WebDriverWait(self.driver, 10).until(EC.alert_is_present())
+
+        # step 4 of the TC_503 (clicks OK)
+        alert.accept()
+        sleep(2)
+
+        # step 5 of the TC_503
+        menu_cart_link = self.driver.find_element(By.ID, "cartur")
+        menu_cart_link.click()
+
+        sleep(2)
+
+        # deifinig prducts added to the cart page
+        products = self.driver.find_elements(By.CSS_SELECTOR, "#tbodyid tr") #number of rows in the table
+
+        # checking if PLACE ORDER button is visible
+        place_order_btn = self.driver.find_element(By.XPATH, "//button[text()='Place Order']")
+
+
+        # Assertions
+        self.assertEqual(len(products), 1)
+        assert place_order_btn.is_displayed()
+
+     # close browser after each test case
+    def tearDown(self):
+        self.driver.quit()
+
+    if __name__ == '__main__':
+        unittest.main()
+
 
 
 
